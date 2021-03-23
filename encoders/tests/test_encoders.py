369e6491ec6aa5ae012f173d790791d80afcbdbf
Meirelles_JL <jldemeirelles@gmail.com>
2021-03-23 20:10:56 -0300

    Added Family category encoder and tests

--- encoders/tests/test_encoders.py
+++ encoders/tests/test_encoders.py
@@ -3,19 +3,21 @@ import numpy as np
 
 
 from dna_encoder import DnaEncoder
+from family_encoder import FamilyEncoder
 
 
 class TestDnaEncoder(unittest.TestCase):
+    """ Class to test DnaEnconder class """
 
     def test_false_input(self):
-        """ Test if encode doesn't accept wrong inputs """
+        """ Test if encoder doesn't accept wrong inputs """
         encoder = DnaEncoder()
         protein_seq = "MNLLLTLLTN"
         with self.assertRaises(Exception):
             encoder.encode(protein_seq)
 
     def test_right_encoding(self):
-        """ Test if encoder is making the right encoding"""
+        """ Test if encoder is making the right encoding """
         encoder = DnaEncoder()
         sequence = "ATCG"
         expected_result = np.array([
@@ -28,6 +30,36 @@ class TestDnaEncoder(unittest.TestCase):
 
         encoded_seq = encoder.encode(sequence)
         equal = np.array_equal(encoded_seq, expected_result)
+        self.assertTrue(equal)
+
+
+class TestFamilyEncoder(unittest.TestCase):
+    """ Class to test FamilyEncoder class """
+
+    def test_false_input(self):
+        """ Test if encoder doesn't accept wrong inputs """
+        encoder = FamilyEncoder()
+        wrong_category = "123"
+        with self.assertRaises(Exception):
+            encoder.encode(wrong_category)
+
+    def test_right_encoding(self):
+        """ Test if encoder is making the right encoding """
+        encoder = FamilyEncoder()
+        category = "Pa"
+        expected_result = [
+            np.array(
+                [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+                 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]
+                ),
+            np.array(
+                [1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
+                 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]
+                ),
+        ]
+
+        encoded_cat = encoder.encode(category)
+        equal = np.array_equal(encoded_cat, expected_result)
         self.assertTrue(equal)
 
 
