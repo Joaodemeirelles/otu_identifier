c80953f50ab13699f0bc2555a889ea6dde0b25b5
Meirelles_JL <jldemeirelles@gmail.com>
2021-06-05 20:28:47 -0300

    Sampling test and train data from dataset

--- otu_identifier/__main__.py
+++ otu_identifier/__main__.py
@@ -6,9 +6,14 @@ from model.models import OtuIdentifyNet
 
 
 dataset = SeqDataset(filename="../domain_test.fasta")
-training = torch.utils.data.DataLoader(dataset, batch_size=50, shuffle=True)
-validating = torch.utils.data.DataLoader(dataset, batch_size=50, shuffle=True)
+train_size = int(len(dataset) * 0.8)
+test_size = len(dataset) - train_size
+train_set, val_set = torch.utils.data.random_split(
+    dataset, [train_size, test_size]
+)
+train_set = torch.utils.data.DataLoader(train_set, batch_size=50, shuffle=True)
+test_set = torch.utils.data.DataLoader(val_set, batch_size=50, shuffle=True)
 model = OtuIdentifyNet()
-trainer = pl.Trainer(gpus=1)
+trainer = pl.Trainer(gpus=-1)
 
-trainer.fit(model, training, validating)
+trainer.fit(model, train_set, test_set)
